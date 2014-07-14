07a0abb80f7d107e4b6410ef2fa6a9ac941d42e8
icerunner <icerunner@gmail.com>
2014-07-14 16:35:27 +0100

    Initial commit
    First version of the plugin

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
-ofq_render_csv
-==============
+# ofq\_render\_csv
 
 A Wordpress plugin to render a local CSV file as a table using a shortcode.
+
+## Usage
+
+    [csv src="/path/to/local_file.csv" disable="header" id="css-hook"]
+
+## Licensing
+
+Copyright (c) Philip McAllister 2014.
+
+This software is licensed under the MIT Licence. For more details see 'LICENCE'.
+
+## Options
+
+### src
+This is the source to the file. It can either be a path to the file, such as `/path/to/file.csv` or it can be a canonical URI such as `http://ofqual.gov.uk/path/to/file.csv`.
+
+    [csv src="/path/to/file.csv"]
+    [csv src="https://my.hostname.com/path/to/file.csv"]        
+
+At present this plugin does not allow you to render CSV files that are hosted anywhere other than on the host this plugin is installed on. It will only render local CSV files. This is to prevent you using someone else's CSV file and it then being edited to display something profane on your site.
+
+If you do want to display remote CSV files, it should be relatively trivial to adapt the code to allow this. Just look for the comment:
+
+    // We will not accept if the file is on a remote host
+
+The format of the file isn't checked and it doesn't care if the file ends in .csv or not.
+
+### disable
+
+This option accepts a comma-separated list of features to disable.
+
+            [csv src="/path/to/file.csv" disable="option1,option2"]
+
+At the moment the options are:
+
+* header
+
+  To stop the first row being used as a table header.
+ 
+See also the table under 'headers' to see how this option behaves in conjunction with the 'headers' option.
+
+### headers
+This option allows you to override any header row in the source file or to provide headers if none exist otherwise. It accepts a comma-separated list of headers. No checking is done to see if the number of cells match the CSV source.
+
+        [csv src="/path/to/file.csv" headers="alpha,beta,gamma,delta,epsilon"]
+
+The effect is dependent on whether you have disabled the header row using the 'disabled' option. The table below shows how the two interact together.
+
+    | disabled="header" | headers="..." | Result                                                                                           |
+    |:-----------------:|--------------:| ------------------------------------------------------------------------------------------------ |
+    | No                | No            | Row 1 from the CSV file is used for the table headers. Data starts at CSV row 2                  |
+    | Yes               | No            | No headers in the table. Data starts at CSV row 1                                                |
+    | No                | Yes           | Use the override headers for the table headers. CSV row 1 is discarded. Data starts at CSV row 2 |
+    | Yes               | Yes           | Use the override headers for the table headers. Data starts at CSV row 1                         |
+
+
+### id
+
+Allows you to set an id attribute on the table element, for CSS or Javascript hooks.
+
+    [csv src="/path/to/file.csv" id="table-a"]
+
+### caption
+
+Provides a table caption.
+
+    [csv src="/path/to/file.csv" caption="Table A: showing something awesome"]
+
+### summary
+
+Allows you to provide an accessible text summary of the table.
+
+    [csv src="/path/to/file.csv" summary="This description is read out by screen readers to give an overview description of what this table shows without having to have all the data read out cell by cell."]
---
+++ ofq_render_csv.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Plugin Name: Ofqual Render CSV
+ * Plugin URI: https://github.com/icerunner/ofq_render_csv
+ * Description: Import data from a CSV spreadsheet and display in a table.
+ * Version: 1.0
+ * Author: Philip McAllister
+ * Author URI: http://about.me/icerunner
+ * Text Domain: ofq_render_csv
+ * License: OGL
+ */
+function csv_shortcode($attrs, $content = null) {
+	
+	$plug_debug = true;
+	$header_row = [];
+	$disable = [];
+	$table_output = "<table";
+	$csv_line = [];
+	
+	
+	// XXX DEBUG
+	//if ($plug_debug) { print "\n\n<!-- CSV: Attributes\n".print_r($attrs)."\n-->\n\n"; }
+	if ($plug_debug) { echo "<!-- CSV: Server is ".$_SERVER['SERVER_NAME']." -->\n"; }
+	
+	// Only process is there is a file src set
+	if (isset($attrs['src']))
+	{
+		// If the src starts with a protocol, deal with it here
+		if (preg_match('/^https?:\/\/(.+?)\/(.+)/',$attrs['src'],$match))
+		{
+			// We will accept if the file is on one of our hosts
+			if ($match[1] == $_SERVER['SERVER_NAME'])
+			{
+				$attrs['src'] = $match[2];
+			}
+			// We will not accept if the file is on an remote host
+			else
+			{
+				return "<!-- CSV: Rendering of remote CSV files is not allowed. Our host is: '".$_SERVER['SERVER_NAME']."' src was: '".$attrs['src']."' -->";
+			}
+		}
+			
+		
+		// Find the local file path of the referenced file
+		$local_csv_path = realpath(ABSPATH . $attrs['src']);
+		
+		// XXX DEBUG
+		if ($plug_debug) { print("<!-- Local CSV file: '".$local_csv_path."' -->\n"); }
+		
+		// Open CSV file	
+		$csv_fh = fopen($local_csv_path,"r");
+		
+		// If file successfully opened for reading
+		if ($csv_fh)
+		{
+			// Split out 'disable' attribute
+			if (array_key_exists('disable',$attrs))
+			{
+				$disable = explode(',',$attrs['disable']);
+			}
+			// Split out the headers
+			if (array_key_exists('headers',$attrs))
+			{
+				$header_row = explode(',',$attrs['headers']);
+			}
+				
+			// Add a table ID if one specified
+			if (array_key_exists('id',$attrs))
+			{
+				$table_output .= ' id="'. $attrs['id'] .'"';
+			}
+				
+			// Add a table summary if one specified
+			if (array_key_exists('summary',$attrs))
+			{
+				$table_output .= ' summary="'. $attrs['summary'] .'"';
+			}
+				
+			$table_output .= ">";
+				
+			// If a caption is provided add it
+			if (array_key_exists('caption',$attrs))
+			{
+				$table_output .= "<caption>" . $attrs['caption'] . "</caption>";
+			}
+			
+			// Logic to control how headers appear
+			// +---------------------+-------------------+---------------------------------------------+
+			// | Header row disabled | Headers overidden |                   Result                    |
+			// +---------------------+-------------------+---------------------------------------------+
+			// |         No          |        No         | Use row 1 as headers. Data starts at row 2. |
+			// +---------------------+-------------------+---------------------------------------------+
+			// |         Yes         |        No         | No headers. Data starts at row 1.           |
+			// +---------------------+-------------------+---------------------------------------------+
+			// |         No          |        Yes        | Use override headers from post or page.     |
+			// |                     |                   | Row 1 discarded. Data starts at row 2.      |
+			// +---------------------+-------------------+---------------------------------------------+
+			// |         Yes         |        Yes        | Use override headers. Data starts at row 1. |
+			// +---------------------+-------------------+---------------------------------------------+
+
+			// XXX DEBUG
+			//if ($plug_debug) { echo "CSV: Header debug info \n".print_r($attrs)."\n".print_r($attrs['disable'])."\n".print_r($headers)."\n-->\n"; }
+
+			if ((!array_key_exists('disable',$attrs) || !in_array('header',$disable)) && !array_key_exists('headers',$attrs)) {  // Header row not disabled and no override headers
+				// Get header row from file
+				$header_row = fgetcsv($csv_fh);
+				// XXX DEBUG
+				if ($plug_debug) { echo "\n<!-- CSV: Header not disabled, no override -->\n"; }
+			}
+			elseif (in_array('header',$disable) && !array_key_exists('headers',$attrs)) { // Header row disabled and no override headers
+				// Do nothing because no headers used
+				// XXX DEBUG
+				if ($plug_debug) { echo "\n<!-- CSV: Header disabled, no override -->\n"; }
+			}
+			elseif ((!array_key_exists('disable',$attrs) || !in_array('header',$disable)) && array_key_exists('headers',$attrs)) { // Header row disabled and override headers provided
+				// Discard the first row from the file
+				$discard_row = fgetcsv($csv_fh);
+				// XXX DEBUG
+				if ($plug_debug) { echo "\n<!-- CSV: Header not disabled, override headers -->\n"; }
+			}
+			elseif (in_array('header',$disable) && array_key_exists('headers',$attrs)) {
+				// Do nothing because override headers set above
+				// XXX DEBUG
+				if ($plug_debug) { echo "\n<!-- CSV: Header disabled, override headers -->\n"; }
+			}
+				
+			// If headers set to be used, display them
+			if (count($header_row) > 0)
+			{	
+				$table_output .= "\n<thead>";
+				
+				foreach ($header_row as $cell)
+				{
+					$table_output .= "<th>".$cell."</th>\n";
+				}
+				$table_output .= "</tr>\n</thead>\n";
+			}
+				
+			// Start the table body
+			$table_output .= "<tbody>\n";
+			// Parse CSV file and render as rows & cells
+			while (($csv_line = fgetcsv($csv_fh)) !== FALSE)
+			{
+				$table_output .= "<tr>\n";
+				foreach ($csv_line as $cell)
+				{
+					//replace empty cell with nonbreaking space
+					if ( !trim($cell) ) {
+ 						 $cell="&nbsp;"; 						 
+					}
+					$table_output .= "<td>".$cell."</td>\n";
+				}
+				$table_output .= "</tr>\n";
+			}
+			$table_output .= "</tbody></table>\n";
+				
+			fclose($csv_fh);
+				
+  		return $table_output;
+  	}
+  	else
+  	{
+  		return "<!-- CSV: Couldn't open file '".$local_csv_path."' couldn't be opened for reading: ".error_get_last()."-->";
+  	}
+  }
+  else
+  {
+  	return '<!-- CSV: No CSV file specified to render -->';
+  }
+}
+
+add_shortcode('csv', 'csv_shortcode');
