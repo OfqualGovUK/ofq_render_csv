b39cb67c6c42e44d61c56b0955ccc5f2c5cbc43d
icerunner <icerunner@gmail.com>
2014-07-16 12:29:14 +0100

    Refactor and additional features
    * The option `disable="headers"` is deprecated and will be removed in a
    future version. Use `disable="CSVColHeaders"` instead.
    * The option `headers` is deprecated and will be removed in a future
    version. Use `colheaders` instead.
    * Added option `enable` that accepts a comma-separated list of features
    to explicitly enable.
    * Added features `CSVColHeaders` and `CSVRowHeaders` to options
    `enable=` and `disable=`.
    * Added option `colheaders` to add user-supplied headers to columns.
    * Added option `rowheaders` to add user supplied headers to rows.
    * Added option `linktosource` to add a link to the source CSV file.
    * Added option `tableclass` to add a class attribute to the `<table>`
    tag.
    * Newlines now preserved in the output by replacing them with `<br>`
    tags.
    * Fixed a bug where the filename would be missing the leading / if a
    URI was given as the CSV source.
    * Fixed a bug that missed the opening `<tr>` after the `<thead>` tag.

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ A Wordpress plugin to render a local CSV file as a table using a shortcode.
 
     [csv src="/path/to/local_file.csv" disable="header" id="css-hook"]
 
+## Version
+
+Version 2.0
+Wednesday 16th July 2014
+
 ## Licensing
 
 Copyright (c) Philip McAllister 2014.
@@ -15,6 +20,27 @@ This software is licensed under the MIT Licence. For more details see 'LICENCE'.
 ## Installation
 
 Copy the file `ofq_render_csv.php` to `wp-content/plugins/ofq_render_csv/` in your wordpress install and then enable it from the admin interface.
+
+## Changes
+
+### v2.0
+
+* The option `disable="headers"` is deprecated and will be removed in a future version. Use `disable="CSVColHeaders"` instead.
+* The option `headers` is deprecated and will be removed in a future version. Use `colheaders` instead.
+* Added option `enable` that accepts a comma-separated list of features to explicitly enable.
+* Added features `CSVColHeaders` and `CSVRowHeaders` to options `enable=` and `disable=`.
+* Added option `colheaders` to add user-supplied headers to columns.
+* Added option `rowheaders` to add user supplied headers to rows.
+* Added option `linktosource` to add a link to the source CSV file.
+* Added option `tableclass` to add a class attribute to the `<table>` tag.
+* Newlines now preserved in the output by replacing them with `<br>` tags.
+* Fixed a bug where the filename would be missing the leading / if a URI was given as the CSV source.
+* Fixed a bug that missed the opening `<tr>` after the `<thead>` tag.
+
+## Notes
+
+* Newlines are preserved from the source CSV file by converting newlines into `<br>` tags.
+* Empty cells are pupulated with a non-breaking space: `&nbsp;`.
 
 ## Options
 
@@ -32,34 +58,73 @@ If you do want to display remote CSV files, it should be relatively trivial to a
 
 The format of the file isn't checked and it doesn't care if the file ends in .csv or not.
 
-### disable
+### enable / disable
 
-This option accepts a comma-separated list of features to disable.
+This option explicitly enables options that would normally be disabled by default, or disable options that are enabled by default.
 
-            [csv src="/path/to/file.csv" disable="option1,option2"]
+    [csv src="/path/to/file.csv" enable="Option1,Option2" disable="Option3,Option4"]
 
-At the moment the options are:
+The options are not case sensitive and are:
 
-* header
+* CSVColHeaders [default=enabled]
 
-  To stop the first row being used as a table header.
- 
-See also the table under 'headers' to see how this option behaves in conjunction with the 'headers' option.
+  Enable this option to indicate that the first row of the CSV file contains headings for the data columns.
+  
+  Disable this option to indicate that the first row of the CSV file is data.
+  
+  Also see the table under `rowheaders` to see how this element reacts in conjunction with other options.
 
-### headers
-This option allows you to override any header row in the source file or to provide headers if none exist otherwise. It accepts a comma-separated list of headers. No checking is done to see if the number of cells match the CSV source.
+* CSVRowHeaders [default=disabled]
 
-        [csv src="/path/to/file.csv" headers="alpha,beta,gamma,delta,epsilon"]
+  Enable this option to indicate that the first cell in every row is a heading for the row. This makes the first data element of any row be displayed as a header with the `scope="row"` attribute added to the `<th>` element.
+  
+  Disable this option to indicate that the first cell in every row is data. The `CSVColHeaders` option overrides this setting, so if you disable `CSVRowHeaders` and enable `CSVColHeaders` The first cell of row 1 will still be treated as a header for the column.
+  
+  Also see the table under `rowheaders` to see how this element reacts in conjunction with other options.
 
-The effect is dependent on whether you have disabled the header row using the 'disabled' option. The table below shows how the two interact together.
+* header (deprecated)
 
-    | disabled="header" | headers="..." | Result                                                                                           |
-    |:-----------------:|--------------:| ------------------------------------------------------------------------------------------------ |
-    | No                | No            | Row 1 from the CSV file is used for the table headers. Data starts at CSV row 2                  |
-    | Yes               | No            | No headers in the table. Data starts at CSV row 1                                                |
-    | No                | Yes           | Use the override headers for the table headers. CSV row 1 is discarded. Data starts at CSV row 2 |
-    | Yes               | Yes           | Use the override headers for the table headers. Data starts at CSV row 1                         |
+  This option is deprecated and has been replaced with `CSVColHeaders"` above.
 
+
+### colheaders
+
+This option allows you to override any header row in the source file or to provide column headers if none exist otherwise. It accepts a comma-separated list of headers. No checking is done to see if the number of cells match the CSV source.
+
+    [csv src="/path/to/file.csv" colheaders="alpha,beta,gamma,delta,epsilon"]
+
+The effect is dependent on the settings of other options. See the table under `rowheaders` for details.
+
+### headers (deprecated)
+
+This option is deprecated and has been replaced with the `colheaders` option above.
+
+### rowheaders
+
+This option allows you to override any header column in the source file or to provide row headers if none exist otherwise. If there are fewer headers specified than rows in the source CSV file, then the header cell for those rows will be left blank. To insert a blank header use `,,` - for example `one,two,,four,,,seven`.
+
+    [csv file="/path/to/file.csv" rowheaders="One,Two,Three,Four,,Six"]
+
+The effect of this option depends on the other settings, such as `enable="CSVColHeaders,CSVRowHeaders"`, `disable="CSVColHeaders,CSVRowHeaders"` and `colheaders="..."`. The table below shows the effects of these different options.
+
+    | CSVColHeaders | colheaders="..." | CSVRowHeaders | rowheaders="..." | Result                                                                                                                 |
+    |:-------------:|:----------------:|:-------------:|:----------------:| ---------------------------------------------------------------------------------------------------------------------- |
+    | Disabled      | No               | Disabled      | No               | No column headers. No row headers. Every cell rendered as data. Data starts at row 1 col 1.                            |
+    | Enabled       | No               | Disabled      | No               | Column headers from row 1. No row headers. Data starts at row 2 col 1.                                                 |
+    | Disabled      | Yes              | Disabled      | No               | Column headers from user. No row headers. Data starts at row 1 col 1.                                                  |
+    | Enabled       | Yes              | Disabled      | No               | Column headers from user. No row headers. First row from CSV discarded. Data starts at row 2 col 1.                    |
+    | Disabled      | No               | Enabled       | No               | No column headers. Row headers from col 1. Data starts at row 1 col 2.                                                 |
+    | Enabled       | No               | Enabled       | No               | Column headers from row 1. Row headers from col 1. Data starts at row 2 col 2.                                         |
+    | Disabled      | Yes              | Enabled       | No               | Column headers from user. Row headers from col 1. Data starts at row 1 col 2.                                          |
+    | Enabled       | Yes              | Enabled       | No               | Column headers from user. Row headers from col 1. Row 1 discarded. Data starts row 2 col 2.                            |
+    | Disabled      | No               | Disabled      | Yes              | No column headers. User-overridden row headers. Data starts at row 1 col 1.                                            |
+    | Enabled       | No               | Disabled      | Yes              | Column headers from row 1, user-overridden row headers. Data starts at row 2 col 1.                                    |
+    | Disabled      | Yes              | Disabled      | Yes              | Column headers from user. Row headers from user. Data starts at row 1 col 1.                                           |
+    | Enabled       | Yes              | Disabled      | Yes              | Column headers from user. Row headers from user. CSV row 1 discarded. Data starts at row 2 col 1.                      |
+    | Disabled      | No               | Enabled       | Yes              | No column headers. User-overridden row headers. First column discarded. Data starts at row 1 col 2.                    |
+    | Enabled       | No               | Enabled       | Yes              | Column headers from line 1. Row headers from user. First column discarded. Data starts row 2 col 2.                    |
+    | Disabled      | Yes              | Enabled       | Yes              | Column headers from user. Row headers from user. First column discarded. Data starts row 1 col 2.                      |
+    | Enabled       | Yes              | Enabled       | Yes              | Column headers from user. Row headers from user. First row discarded. First column discarded. Data starts row 2 col 2. |
 
 ### id
 
@@ -78,3 +143,19 @@ Provides a table caption.
 Allows you to provide an accessible text summary of the table.
 
     [csv src="/path/to/file.csv" summary="This description is read out by screen readers to give an overview description of what this table shows without having to have all the data read out cell by cell."]
+
+### linktosource
+
+Allows you to link to the source CSV file, and specify the text for the link.
+
+    [csv src="/path/to/file.csv" linktosource="Source data file"]
+
+This will produce the code `<p><a href="/path/to/file.csv" class="tabledatasource">Source data file</a></p>`
+
+### tableclass
+
+Add a class attribute to the `<table>` tag.
+
+    [csv src="/path/to/file.csv" tableclass="sortable"]
+
+This will produce the code `<table class="sortable">`
--- ofq_render_csv.php
+++ ofq_render_csv.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ofqual Render CSV
  * Plugin URI: https://github.com/icerunner/ofq_render_csv
  * Description: Import data from a CSV spreadsheet and display in a table.
- * Version: 1.0
+ * Version: 2.0
  * Author: Philip McAllister
  * Author URI: http://about.me/icerunner
  * Text Domain: ofq_render_csv
@@ -11,11 +11,16 @@
  */
 function csv_shortcode($attrs, $content = null) {
 	
-	$plug_debug = true;
-	$header_row = [];
+	$plug_debug = false;
+	$csv_file = "";
+	$headers_for_cols = [];
+	$headers_for_rows = [];
+	$user_row_headers = false;
+	$enable = [];
 	$disable = [];
 	$table_output = "<table";
 	$csv_line = [];
+	$col_num = 0;
 	
 	
 	// XXX DEBUG
@@ -26,12 +31,15 @@ function csv_shortcode($attrs, $content = null) {
 	if (isset($attrs['src']))
 	{
 		// If the src starts with a protocol, deal with it here
-		if (preg_match('/^https?:\/\/(.+?)\/(.+)/',$attrs['src'],$match))
+		if (preg_match('/^https?:\/\/(.+?)(\/.+)/',$attrs['src'],$match))
 		{
 			// We will accept if the file is on one of our hosts
 			if ($match[1] == $_SERVER['SERVER_NAME'])
 			{
 				$attrs['src'] = $match[2];
+				
+				// Find the local file path of the referenced file
+				$csv_file = realpath(ABSPATH . $attrs['src']);
 			}
 			// We will not accept if the file is on an remote host
 			else
@@ -39,31 +47,62 @@ function csv_shortcode($attrs, $content = null) {
 				return "<!-- CSV: Rendering of remote CSV files is not allowed. Our host is: '".$_SERVER['SERVER_NAME']."' src was: '".$attrs['src']."' -->";
 			}
 		}
-			
-		
-		// Find the local file path of the referenced file
-		$local_csv_path = realpath(ABSPATH . $attrs['src']);
 		
 		// XXX DEBUG
-		if ($plug_debug) { print("<!-- Local CSV file: '".$local_csv_path."' -->\n"); }
+		if ($plug_debug) { print("<!-- CSV: File is: '".$csv_file."' -->\n"); }
 		
 		// Open CSV file	
-		$csv_fh = fopen($local_csv_path,"r");
+		$csv_fh = fopen($csv_file,"r");
 		
 		// If file successfully opened for reading
 		if ($csv_fh)
 		{
+			// Split out 'enable' attribute
+			if (array_key_exists('enable',$attrs))
+			{
+				$enable = explode(',',$attrs['enable']);
+				// Put enables features in the array as lowercase keys
+				$enable = array_combine(array_map('strtolower', $enable), $enable);
+			}
+			
 			// Split out 'disable' attribute
 			if (array_key_exists('disable',$attrs))
 			{
 				$disable = explode(',',$attrs['disable']);
+				// Put enables features in the array as lowercase keys
+				$disable = array_combine(array_map('strtolower', $disable), $disable);
+				
+				// XXX DEPRECATED
+				// Deal with deprecated option
+				if (array_key_exists('header',$disable))
+				{
+					$disable['csvcolheaders'] = 'csvcolheaders';
+				}
 			}
-			// Split out the headers
+			
+			// XXX DEPRECATED
+			// Split out the headers - replaced by colheaders
 			if (array_key_exists('headers',$attrs))
 			{
-				$header_row = explode(',',$attrs['headers']);
+				if (!array_key_exists('colheaders',$attrs))
+				{
+					$attrs['colheaders'] = $attrs['headers'];
+				}
 			}
-				
+			
+			// Split out the column headers - takes precedence over 'headers' if both exist
+			if (array_key_exists('colheaders',$attrs))
+			{
+				$headers_for_cols = explode(',',$attrs['colheaders']);
+			}
+			
+			// Split out the row headers
+			if (array_key_exists('rowheaders',$attrs))
+			{
+				$headers_for_rows = explode(',',$attrs['rowheaders']);
+				$user_row_headers = true;
+			}
+			
 			// Add a table ID if one specified
 			if (array_key_exists('id',$attrs))
 			{
@@ -75,7 +114,13 @@ function csv_shortcode($attrs, $content = null) {
 			{
 				$table_output .= ' summary="'. $attrs['summary'] .'"';
 			}
-				
+			
+			
+			// Add a class to the table tag if one specified
+			if(array_key_exists('tableclass',$attrs))
+			{
+				$table_output .= ' class="'. $attrs['tableclass'] .'"';
+			}
 			$table_output .= ">";
 				
 			// If a caption is provided add it
@@ -85,53 +130,67 @@ function csv_shortcode($attrs, $content = null) {
 			}
 			
 			// Logic to control how headers appear
-			// +---------------------+-------------------+---------------------------------------------+
-			// | Header row disabled | Headers overidden |                   Result                    |
-			// +---------------------+-------------------+---------------------------------------------+
-			// |         No          |        No         | Use row 1 as headers. Data starts at row 2. |
-			// +---------------------+-------------------+---------------------------------------------+
-			// |         Yes         |        No         | No headers. Data starts at row 1.           |
-			// +---------------------+-------------------+---------------------------------------------+
-			// |         No          |        Yes        | Use override headers from post or page.     |
-			// |                     |                   | Row 1 discarded. Data starts at row 2.      |
-			// +---------------------+-------------------+---------------------------------------------+
-			// |         Yes         |        Yes        | Use override headers. Data starts at row 1. |
-			// +---------------------+-------------------+---------------------------------------------+
+			//
+            // For details of the various combinations that affect how headers appear see https://github.com/icerunner/ofq_render_csv/blob/master/README.md
+			
+            // XXX DEBUG
+			//if ($plug_debug) { echo "CSV: Header debug info \n".print_r($attrs)."\n".print_r($disable)."\n-->\n"; }
 
-			// XXX DEBUG
-			//if ($plug_debug) { echo "CSV: Header debug info \n".print_r($attrs)."\n".print_r($attrs['disable'])."\n".print_r($headers)."\n-->\n"; }
-
-			if ((!array_key_exists('disable',$attrs) || !in_array('header',$disable)) && !array_key_exists('headers',$attrs)) {  // Header row not disabled and no override headers
+			/*
+			 * LOGIC TO CONTROL COLUMN HEADERS
+			 */
+			if ((!array_key_exists('disable',$attrs) || !array_key_exists('csvcolheaders',$disable)) && !array_key_exists('colheaders',$attrs))  // Header row not disabled and no override headers
+			{
 				// Get header row from file
-				$header_row = fgetcsv($csv_fh);
+				$headers_for_cols = fgetcsv($csv_fh);
+				// If row headers also enabled, knock out the first header
+				if (array_key_exists('csvrowheaders',$enable))
+				{
+					array_shift($headers_for_cols);
+				}
 				// XXX DEBUG
 				if ($plug_debug) { echo "\n<!-- CSV: Header not disabled, no override -->\n"; }
 			}
-			elseif (in_array('header',$disable) && !array_key_exists('headers',$attrs)) { // Header row disabled and no override headers
+			elseif (array_key_exists('csvcolheaders',$disable) && !array_key_exists('colheaders',$attrs)) // Header row disabled and no override headers
+			{
 				// Do nothing because no headers used
 				// XXX DEBUG
 				if ($plug_debug) { echo "\n<!-- CSV: Header disabled, no override -->\n"; }
 			}
-			elseif ((!array_key_exists('disable',$attrs) || !in_array('header',$disable)) && array_key_exists('headers',$attrs)) { // Header row disabled and override headers provided
+			elseif((!array_key_exists('disable',$attrs) || array_key_exists('csvcolheaders',$enable) || !array_key_exists('csvcolheaders',$disable)) && array_key_exists('colheaders',$attrs)) // header row not disabled, no override headers
+			{
 				// Discard the first row from the file
 				$discard_row = fgetcsv($csv_fh);
 				// XXX DEBUG
 				if ($plug_debug) { echo "\n<!-- CSV: Header not disabled, override headers -->\n"; }
 			}
-			elseif (in_array('header',$disable) && array_key_exists('headers',$attrs)) {
+			elseif (array_key_exists('csvcolheaders',$disable) && array_key_exists('colheaders',$attrs)) { // Header row disabled and override headers
 				// Do nothing because override headers set above
 				// XXX DEBUG
 				if ($plug_debug) { echo "\n<!-- CSV: Header disabled, override headers -->\n"; }
 			}
-				
-			// If headers set to be used, display them
-			if (count($header_row) > 0)
+			
+			
+			/*
+			 * LOGIC TO CONTROL ROW HEADERS
+			 */
+			
+			
+			// If column headers set to be used, display them
+			if (count($headers_for_cols) > 0)
 			{	
-				$table_output .= "\n<thead>";
 				
-				foreach ($header_row as $cell)
+				$table_output .= "\n<thead><tr>";
+				
+				// If we also have row headers, insert a blank cell
+				if (array_key_exists('rowheaders',$attrs) || (array_key_exists('csvrowheaders',$enable) && !array_key_exists('csvrowheaders',$disable)))
 				{
-					$table_output .= "<th>".$cell."</th>\n";
+					$table_output .= "<td>&nbsp;</td>\n";
+				}
+				
+				foreach ($headers_for_cols as $cell)
+				{
+					$table_output .= "<th>".nl2br($cell)."</th>\n";
 				}
 				$table_output .= "</tr>\n</thead>\n";
 			}
@@ -141,20 +200,62 @@ function csv_shortcode($attrs, $content = null) {
 			// Parse CSV file and render as rows & cells
 			while (($csv_line = fgetcsv($csv_fh)) !== FALSE)
 			{
+				$col_num = 0;
 				$table_output .= "<tr>\n";
 				foreach ($csv_line as $cell)
 				{
 					//replace empty cell with nonbreaking space
-					if ( !trim($cell) ) {
+					if ( !trim($cell) )
+					{
  						 $cell="&nbsp;"; 						 
 					}
-					$table_output .= "<td>".$cell."</td>\n";
+					// If user-set row headers
+					if (!$col_num && $user_row_headers ) 
+					{
+						$th = array_shift($headers_for_rows);
+						
+						if ( !trim($th) || !isset($th))
+						{
+ 						 	$th="&nbsp;"; 						 
+						}
+						var_dump($th);
+						
+						$table_output .= "<th scope=\"row\">".nl2br($th)."</th>";
+						
+						// If we're not discarding the first column, output it
+						if (!array_key_exists('csvrowheaders',$enable))
+						{
+							$table_output .= "<td>".nl2br($cell)."</td>";
+						}
+					}
+					// If not user-set row headers, but using headers from CSV column 1
+					elseif (!$col_num && array_key_exists('csvrowheaders',$enable))
+					{
+						$table_output .= "<th scope=\"row\">".nl2br($cell)."</th>\n";
+					}
+					// Otherwise this is just a normal data cell
+					else
+					{
+						$table_output .= "<td>".nl2br($cell)."</td>\n";
+					}
+
+					$col_num++;
 				}
 				$table_output .= "</tr>\n";
 			}
-			$table_output .= "</tbody></table>\n";
-				
 			fclose($csv_fh);
+			
+			$table_output .= "</tbody></table>\n";
+			
+			if (array_key_exists('linktosource',$attrs))
+			{
+				if (!trim($attrs['linktosource']) || !isset($attrs['linktosource']))
+				{
+					$attrs['linktosource'] = "Source data file";
+				}
+				$table_output .= "<p><a href=\"".$attrs['src']."\" class=\"tabledatasource\">".$attrs['linktosource']."</a></p>\n";
+			}
+			
 				
   		return $table_output;
   	}
